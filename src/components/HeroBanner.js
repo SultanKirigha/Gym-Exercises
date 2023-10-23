77228a094edc85e396b1f3e8300829390898b04a
SultanKirigha <sultankirigha@gmail.com>
2023-10-23 11:55:54 +0300

    Created a responsive show results part

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+import HeroBannerImage from '../assets/images/banner 5.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
@@ -14,7 +14,7 @@ const HeroBanner = () => (
       Check out the most effective exercises personalized to you
     </Typography>
     <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '18px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
     </Stack>
     <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '150px' }}>
       Exercise
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <Stack direction="row" justifyContent="space-around" sx={{gap:{sm:'122px', xs:'20px'}, justifyContent:'none'}} px="20px">
         <Link to='/'>
-            <img src={Logo} alt ="Logo" style={{width:'48px', height:'48px', margin:'0 20]px'}}/>
+            <img src={Logo} alt ="Logo" style={{width:'48px', height:'48px', margin:'0 20px'}}/>
         </Link>
         <Stack direction='row' gap="40px" fontSize="24px" alignItems="flex-end">
             <Link to='/' style={{textDecoration:'none',color:'#3A1212', borderBottom:'3px solid #FF2625'}}> Home</Link>
